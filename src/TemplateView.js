a051125ccde31a73163b460b0c1b6c8dded41c34
Jay Phelps <jay@jayphelps.com>
2012-05-05 23:06:43 -0700

    added starting point for the ML.TemplateView port

---
+++ src/TemplateView.js
@@ -0,0 +1,27 @@
+MLImport("Class.js");
+MLImport("View.js");
+
+(function (window, document) {
+
+    /**
+     * No documentation available yet.
+     *
+     * @class
+     * @name        ML.TemplateView 
+     * @extends     ML.View
+     * @author      Jay Phelps
+     * @since       0.1
+     */
+    var TemplateView = ML.Class.create({
+
+        extend: ML.View
+
+    }, /** @lends ML.TemplateView# */ {
+
+        __construct: function () {
+            
+        }
+
+    });
+
+})(window, document);
