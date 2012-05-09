e3fa461c90792c491f27acdf8642e7f8bb077baf
Jay Phelps <jay@jayphelps.com>
2012-05-08 23:58:42 -0700

    refactored ML.Application yet again. Went with a more SproutCore-like approach as it makes more sense.

--- src/Application.js
+++ src/Application.js
@@ -13,60 +13,54 @@ MLImport("Pane.js");
      */
     var Application = ML.Class.create({
 
+        autoStart: NO,
+        delegateName: "ApplicationDelegate",
         keyPane: null,
         mainPane: null,
+        delegate: null,
         panes: [],
 
-        __construct: function () {
+        __construct: function (applicationObject) {
             if (Application.sharedApplication) throw Error("MLKit: Only one application can run at a time");
+
             Application.sharedApplication = this;
+
+            if (this.autoStart) {
+                this.start();
+            }
+
+            return this;
         },
 
-        initWithDelegate: function (delegateInstance) {
-            this.delegateInstance = delegateInstance;
-            return this;
-        }
+        start: function () {
+            var application = this;
 
+            ML.RootResponder.setup();
+
+            ML.DOM.onReady(function () {
+                console.log(application)
+                var delegateClass = application[application.delegateName];
+
+                if (!delegateClass || !delegateClass.isClass || !ML.isFunction(delegateClass)) throw Error("Application Delegate missing or not a class");
+
+                var delegate = new delegateClass();
+                application.delegate = delegate;
+                
+                // Clear the body so it"s clean slate for the app
+                document.body.innerHTML = "";
+                
+                // Notify app they are ready
+                delegate.applicationDidFinishLaunching(application);
+            });
+        }
     });
 
     Application.sharedApplication = null;
 
+    Application.create = function (members) {
+        return new (ML.Class.create({ extend: ML.Application }, members));
+    };
+
     ML.Application = Application;
-
-    /**
-     * FIXME: Description needed.
-     *
-     * @function        
-     * @author      Jay Phelps
-     * @since       0.1
-     */
-    var startApplication = function (options) {
-        var appNamespace = options.namespace || window;
-        var delegate = appNamespace[options.delegate];
-
-        if (!delegate || !delegate.isClass || !ML.isFunction(delegate)) throw Error("Application Delegate missing or not a class");
-
-        var delegateInstance = new delegate();
-
-        var application = (new ML.Application).initWithDelegate(delegateInstance);
-
-        var applicationDidFinishLaunching = ML.bind(
-            delegateInstance.applicationDidFinishLaunching,
-            delegateInstance,
-            application
-        );
-
-        ML.RootResponder.setup();
-
-        ML.DOM.onReady(function () {
-            // Clear the body so it"s clean slate for the app
-            document.body.innerHTML = "";
-            
-            // Notify app they are ready
-            applicationDidFinishLaunching();
-        });
-    }
-
-    ML.startApplication = startApplication;
 
 })(window, document);
